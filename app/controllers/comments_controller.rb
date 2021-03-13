816865a8337d7a7bb308f97729d8c856697f8258
neruko40 <0ppa1oqqai@gmail.com>
2021-03-13 20:40:29 +0900

    Comennt function validates fixed

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -4,9 +4,9 @@ class CommentsController < ApplicationController
   def create
     @comment = current_user.comments.new(comment_params)#こっちもcommentのcは小文字
     if @comment.save
-      redirect_back(fallback_location: root_path)
+      redirect_back(fallback_location: root_path, notice: 'コメントを投稿しました。')
     else
-      redirect_back(fallback_location: root_path)
+      redirect_back(fallback_location: root_path, notice: 'コメントの投稿に失敗しました。')
     end
   end
 
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,6 @@
 class Comment < ApplicationRecord
+  validates :comment_content, presence: true
+
   belongs_to :user #Comment.userでコメントの所有者を取得
   belongs_to :post #Comment.postでそのコメントがされた投稿を取得
 end
